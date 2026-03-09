92a5ce17fce698332ec4cd2cdbd778c51299224a
Eduardo P-H <eduardo_perez_hickman@yahoo.com>
2026-03-09 19:40:40 +0100

    Bug fixes (terraform plan-5)

--- iam.tf
+++ iam.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role" "iam_role" {
 	assume_role_policy = <<-EOF
 	{
 		"Version" : "2012-10-17",
-		"Statement" [
+		"Statement" : [
 		{
 			"Action" : "sts:AssumeRole"
 			"Principal" : {
